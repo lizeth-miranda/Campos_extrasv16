41c8771eed51e7dd2bfa0736d047b5c59c202eb3
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2023-07-14 17:14:49 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Campos Extrasv16',
-    'version': '16.1',
+    'version': '16.2',
     'author': 'Demsa Industrial',
     'website': '',
     'license': 'LGPL-3',
