0cb71db985dfdc6a387f36185c99a97052279cde
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2023-07-14 16:58:02 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,7 @@
     'version': '16.1',
     'author': 'Demsa Industrial',
     'website': '',
+    'license': 'LGPL-3',
     'depends': ['base', 'purchase_requisition', 'account', 'stock'],
     'data': [
         # security
