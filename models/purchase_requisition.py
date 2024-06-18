5eb0e1342766aa78da218f0f1dc3672b6a636672
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2024-06-18 16:51:58 -0600

    Update purchase_requisition.py

--- models/purchase_requisition.py
+++ models/purchase_requisition.py
@@ -3,20 +3,14 @@
 from odoo import fields, models
 
 
+
 class acuerdo_compra(models.Model):
-    _inherit = "purchase.requisition"
+    _inherit = 'purchase.requisition'
 
-    #state = fields.Selection(selection_add=[(
-        #"Autorizar", "Autorizado"), ("ongoing",)])
-    #state_blanket_order = fields.Selection(selection_add=[(
-        #"Autorizar", "Autorizado"), ("ongoing",)])
+    state = fields.Selection(selection_add=[('almacen', 'Almacen'),('done',)], ondelete={'almacen': 'set default'})
+    state_blanket_order = fields.Selection(selection_add=[('almacen', 'Almacen'),('done',)])
     
-    usuario = fields.Many2one(
-        'res.users', string='Usuario',
-        default=lambda self: self.env.user)
-    
-    #def action_autorizar(self):
-        #self.write({'state': 'Autorizar'})
-
-    #doc_ori = fields.Many2one('Documento Origen Prueba', 'documento.origen',)
-    #notas = fields.Text(string="Notas",)
+    def action_almacen(self):
+        print("button clicked!!!")
+        for rec in self:
+            rec.state = 'almacen'
